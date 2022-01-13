9214450392b8cbba0b9180bf06252093358b59b9
nachelysanchez <nashely-5@hotmail.com>
2022-01-13 16:10:33 -0400

    Creando el contexto y agregando migración inicial

---
+++ DAL/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks_System.Entidades;
+
+namespace Tasks_System.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tareas> Tareas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA/Tasks_System.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tareas>().HasData(new Tareas
+            {
+                TareaId = 1,
+                NombreTarea = "Análisis",
+                Descripcion = "La persona X debe de analizar el sistema que su superior le asignó."
+            });
+        }
+    }
+}
---
+++ Migrations/20220113200934_Migracion_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tasks_System.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    TareaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreTarea = table.Column<string>(type: "TEXT", nullable: true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.TareaId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tareas",
+                columns: new[] { "TareaId", "Descripcion", "NombreTarea" },
+                values: new object[] { 1, "La persona X debe de analizar el sistema que su superior le asignó.", "Análisis" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
