0947536ee8db38fe0dee1f5e9ab186f14d784f27
nachelysanchez <nashely-5@hotmail.com>
2022-01-13 17:50:11 -0400

    Iniciando a programar el botón Buscar

--- UI/Registros/rTareas.xaml.cs
+++ UI/Registros/rTareas.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Tasks_System.BLL;
+using Tasks_System.Entidades;
 
 namespace Tasks_System.UI.Registros
 {
@@ -19,14 +21,47 @@ namespace Tasks_System.UI.Registros
     /// </summary>
     public partial class rTareas : Window
     {
+        private Tareas tarea = new Tareas(); 
         public rTareas()
         {
             InitializeComponent();
+            this.DataContext = tarea;
+        }
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = tarea;
         }
 
+        private void Limpiar()
+        {
+            this.tarea = new Tareas();
+            this.DataContext = tarea;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+            if (NombreTareaTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Debe de completar el campo 'Nombre de Tarea'", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                NombreTareaTextBox.Focus();
+            }
+            else if (DescripcionTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Debe de completar el campo 'Descripción de la tarea'", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                DescripcionTextBox.Focus();
+            }
+            return esValido;
+        }
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var tareas = TareasBLL.Buscar(Utilidades.ToInt(TareaIdTextBox.Text));
+            
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
